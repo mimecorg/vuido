2a1629ab80279d681b79d53b223c14ab4569f01b
Aleksandr Tarabaka <aleksandr.tarabaka@privatbank.ua>
2018-05-23 18:36:13 +0300

    updated example

--- example/components/MainWindow.vue
+++ example/components/MainWindow.vue
@@ -1,59 +1,92 @@
 <template>
-  <Window title="Vuido Example" width="400" height="120" margined v-on:close="exit">
-    <Box padded>
-      <Box horizontal padded>
-        <Button stretchy v-on:click="switchMode">Switch mode</Button>
-        <Button stretchy v-on:click="toggleEnabled">Toggle enabled</Button>
-      </Box>
-      <Box v-if="counterMode" horizontal padded>
-        <Text stretchy>Counter: {{ counter }}</Text>
-        <Button v-bind:enabled="enabled" v-on:click="decrement">Decrement</Button>
-        <Button v-bind:enabled="enabled" v-on:click="increment">Increment</Button>
-      </Box>
-      <Box v-else horizontal padded>
-        <Text stretchy>Value: {{ random }}</Text>
-        <Button v-bind:enabled="enabled" v-on:click="randomize">Randomize</Button>
-      </Box>
-      <Box horizontal padded>
-        <TextInput stretchy v-model="text"/>
-        <Text stretchy>{{ text }}</Text>
-      </Box>
+  <Window title="Vuido Control Gallery" width="640" height="480" margined v-on:close="exit">
+    <Box horizontal>
+      <Group title="Vuido Demo" margined>
+        <Box padded>
+          <Box horizontal padded>
+            <Button stretchy @click="switchMode">Switch mode</Button>
+            <Button stretchy @click="toggleEnabled">Toggle enabled</Button>
+          </Box>
+          <Box v-if="counterMode" horizontal padded>
+            <Text stretchy>Counter: {{ counter }}</Text>
+            <Button :enabled="enabled" @click="decrement">Decrement</Button>
+            <Button :enabled="enabled" @click="increment">Increment</Button>
+          </Box>
+          <Box v-else horizontal padded>
+            <Text stretchy>Value: {{ random }}</Text>
+            <Button :enabled="enabled" @click="randomize">Randomize</Button>
+          </Box>
+          <Box horizontal padded>
+            <TextInput stretchy v-model="text"/>
+            <Text stretchy>{{ text }}</Text>
+          </Box>
+        </Box>
+      </Group>
+
+      <Group title="Basic Controls" margined>
+        <Box padded>
+          <Box horizontal padded>
+            <Button stretchy>Button enabled</Button>
+            <Button stretchy :enabled="false">Button disabled</Button>
+          </Box>
+          <Box horizontal padded>
+            <TextInput stretchy v-model="text"/>
+            <TextInput stretchy v-model="text" :enabled="false"/>
+          </Box>
+          <Box horizontal padded>
+            <Checkbox text="Checkbox 1" @toggled="onToggled" checked/>
+            <Checkbox text="Checkbox 2" :enabled="!isChecked"/>
+          </Box>
+          <Box horizontal padded>
+            <Text stretchy>Label</Text>
+            <ColorButton @changed="onColorChanged" :color="color"/>
+          </Box>
+        </Box>
+      </Group>
     </Box>
   </Window>
 </template>
 
 <script>
-import libui from 'libui-node'
+  import libui from 'libui-node'
 
-export default {
-  data() {
-    return {
-      counterMode: true,
-      enabled: true,
-      counter: 0,
-      random: 0,
-      text: 'Edit me'
-    };
-  },
-  methods: {
-    switchMode() {
-      this.counterMode = !this.counterMode;
+  export default {
+    data() {
+      return {
+        counterMode: true,
+        enabled: true,
+        counter: 0,
+        random: 0,
+        text: 'Edit me',
+        isChecked : true,
+        color: '#ffeeff'
+      };
     },
-    toggleEnabled() {
-      this.enabled = !this.enabled;
-    },
-    increment() {
-      this.counter++;
-    },
-    decrement() {
-      this.counter--;
-    },
-    randomize() {
-      this.random = Math.floor( Math.random() * 1000 );
-    },
-    exit() {
-      libui.stopLoop();
+    methods: {
+      switchMode() {
+        this.counterMode = !this.counterMode;
+      },
+      toggleEnabled() {
+        this.enabled = !this.enabled;
+      },
+      increment() {
+        this.counter++;
+      },
+      decrement() {
+        this.counter--;
+      },
+      randomize() {
+        this.random = Math.floor(Math.random() * 1000);
+      },
+      onToggled(){
+        this.isChecked = !this.isChecked;
+      },
+      onColorChanged(val) {
+        this.color = val;
+      },
+      exit() {
+        libui.stopLoop();
+      }
     }
   }
-}
 </script>
