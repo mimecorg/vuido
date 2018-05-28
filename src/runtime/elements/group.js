af99cee4cf4dd2912ba7026465e7ef8d2e9be67b
mimecorg <mimec@mimec.org>
2018-05-28 21:31:25 +0200

    ensure that Group contains one static child widget

--- src/runtime/elements/group.js
+++ src/runtime/elements/group.js
@@ -1,5 +1,6 @@
 import libui from 'libui-node'
 
+import { Element } from './element'
 import { Widget } from './widget'
 
 export class Group extends Widget {
@@ -12,39 +13,48 @@ export class Group extends Widget {
   }
 
   appendChild( childNode ) {
-    super.appendChild( childNode );
+    Element.prototype.appendChild.call( this, childNode );
 
     if ( !( childNode instanceof Widget ) )
       throw new Error( 'Group can only contain child widget' );
 
     if ( this.childNodes.length > 1 )
       throw new Error( 'Group can only contain one child element' );
+
+    if ( this.widget != null )
+      throw new Error( 'Group child element cannot be inserted dynamically' );
+  }
+
+  insertBefore( childNode, referenceNode ) {
+    throw new Error( 'Group child element cannot be inserted dynamically' );
+  }
+
+  removeChild( childNode ) {
+    throw new Error( 'Group child element cannot be removed dynamically' );
   }
 
   _createWidget() {
     this.widget = new libui.UiGroup();
   }
 
-  _appendWidget(childNode) {
-    this.widget.setChild(childNode.widget);
+  _appendWidget( childNode ) {
+    this.widget.setChild( childNode.widget );
   }
 
   _initializeWidgetAttributes() {
     super._initializeWidgetAttributes();
 
-    if (this.attributes.title != '')
-      this.widget.title = this.attributes.title;
-    if (this.attributes.margined)
+    this.widget.title = this.attributes.title;
+    if ( this.attributes.margined )
       this.widget.margined = true;
   }
 
-  _setWidgetAttribute(key, value) {
-    if (key == 'title') {
-      if (this.widget.title != value)
-        this.widget.title = value;
-    } else if (key == 'margined')
+  _setWidgetAttribute( key, value ) {
+    if ( key == 'title' )
+      this.widget.title = value;
+    else if ( key == 'margined' )
       this.widget.margined = value;
     else
-      super._setWidgetAttribute(key, value);
+      super._setWidgetAttribute( key, value );
   }
 }
