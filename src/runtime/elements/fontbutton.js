f1f39421cd21a7f1fe220657acbd5a31c54f8276
mimecorg <mimec@mimec.org>
2018-05-28 18:26:48 +0200

    remove the font attribute from FontButton because it's read-only

--- src/runtime/elements/fontbutton.js
+++ src/runtime/elements/fontbutton.js
@@ -3,41 +3,22 @@ import libui from 'libui-node'
 import {Widget} from './widget'
 
 export class FontButton extends Widget {
-  getDefaultAttributes() {
-    return {
-      ...super._getDefaultAttributes(),
-      font: ''
-    };
-  }
-
   _createWidget() {
     this.widget = new libui.UiFontButton();
   }
 
-  _initializeWidgetAttributes() {
-    super._initializeWidgetAttributes();
-
-    if (this.attributes.font != '')
-      this.widget.font = this.attributes.font;
-  }
-
-  _setWidgetAttribute(key, value) {
-    if (key == 'font') {
-      if (this.widget.font != value)
-        this.widget.font = value;
-    } else {
-      super._setWidgetAttribute(key, value);
-    }
-  }
-
   _setWidgetHandler( event, handler ) {
-    if ( event == 'changed' ){
+    if ( event == 'change' ){
       this.widget.onChanged( () => {
-        handler( this.widget.getFont() ); // return FontDescriptor()
+        handler( this.widget.font );
       } );
     } else {
       super._setWidgetHandler( event, handler );
     }
   }
 
+  get font() {
+    if ( this.widget != null )
+      return this.widget.font;
+  }
 }
