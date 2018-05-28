d595a17327f19cad4ce8628a5e2ae995b4de8b21
mimecorg <mimec@mimec.org>
2018-05-28 18:33:49 +0200

    remove unsupported value attribute from date/time picker widgets

--- src/runtime/elements/datepicker.js
+++ src/runtime/elements/datepicker.js
@@ -1,32 +1,9 @@
 import libui from 'libui-node'
 
-import {Widget} from './widget'
+import { Widget } from './widget'
 
 export class DatePicker extends Widget {
-  getDefaultAttributes() {
-    return {
-      ...super._getDefaultAttributes(),
-      value: ''
-    };
-  }
-
   _createWidget() {
     this.widget = new libui.UiDatePicker();
-  }
-
-  _initializeWidgetAttributes() {
-    super._initializeWidgetAttributes();
-
-    if (this.attributes.value != '')
-      this.widget.text = this.attributes.value;
-  }
-
-  _setWidgetAttribute(key, value) {
-    if (key == 'value') {
-      if (this.widget.text != value)
-        this.widget.text = value;
-    } else {
-      super._setWidgetAttribute(key, value);
-    }
   }
 }
